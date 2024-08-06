9067ce1309f79a2c1c3ff5a653974495a539453f
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-08-06 16:43:47 +0530

    refact: fastapi server

--- app.py
+++ app.py
@@ -1,47 +1,26 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from starlette.middleware.cors import CORSMiddleware
 
-from openai import AsyncOpenAI
-import chainlit as cl
-import os
-from dotenv import load_dotenv
+from chainlit.auth import create_jwt
+from chainlit.user import User
+from chainlit.utils import mount_chainlit
 
-load_dotenv()
+app = FastAPI()
 
-client = AsyncOpenAI(api_key=os.environ["OPENAI_API_KEY"])
-
-
-settings = {
-    "model": "gpt-3.5-turbo",
-    "temperature": 0.7,
-    "max_tokens": 500,
-    "top_p": 1,
-    "frequency_penalty": 0,
-    "presence_penalty": 0,
-}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@cl.on_chat_start
-def start_chat():
-    cl.user_session.set(
-        "message_history",
-        [{"role": "system", "content": "You are a helpful assistant."}],
-    )
+@app.get("/custom-auth")
+async def custom_auth():
+    # Verify the user's identity with custom logic.
+    token = create_jwt(User(identifier="Test User"))
+    return JSONResponse({"token": token})
 
-
-@cl.on_message
-async def main(message: cl.Message):
-    message_history = cl.user_session.get("message_history")
-    message_history.append({"role": "user", "content": message.content})
-
-    msg = cl.Message(content="")
-    await msg.send()
-
-    stream = await client.chat.completions.create(
-        messages=message_history, stream=True, **settings
-    )
-
-    async for part in stream:
-        if token := part.choices[0].delta.content or "":
-            await msg.stream_token(token)
-
-    message_history.append({"role": "assistant", "content": msg.content})
-    await msg.update()
+mount_chainlit(app=app, target="cl_app.py", path="/chainlit")
